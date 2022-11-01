3d39bf2f4fc2b803cf98689e37f9e359eb395649
siddharthDyamgond <104525330+siddharthDyamgond@users.noreply.github.com>
2022-11-01 23:12:01 +0530

    Add on

--- js/Scripts.js
+++ js/Scripts.js
@@ -5,3 +5,8 @@ function precal() {
     document.getElementById("pr").value=((p1/p2)*100);
 }
 
+function ctop() {
+    var p1=document.getElementById("cn").value;
+    document.getElementById("cr").value=(7.1 * p1) + 11
+}
+
