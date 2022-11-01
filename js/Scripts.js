3a44415c0b116b40a290d39500c60222101d20ee
Siddharth Dyamgond <81026894+Siddharth2391@users.noreply.github.com>
2022-11-01 23:00:46 +0530

    Add files via upload

---
+++ js/Scripts.js
@@ -0,0 +1,7 @@
+// Precentage calculator
+function precal() {
+    var p1=document.getElementById("pn").value;
+    var p2=document.getElementById("pt").value;
+    document.getElementById("pr").value=((p1/p2)*100);
+}
+
